dffcefd28be2c3bfd4224d03beaab1161cfc5346
luzialeticia <leticialuzia@tutamail.com>
2020-10-11 23:12:13 -0300

    Criando um objeto de exemplo

--- src/models/postsModels.js
+++ src/models/postsModels.js
@@ -1,5 +1,13 @@
 const helper = require('../helpers/helper')
 
-const posts = []
+const posts = [
+  {
+    id: 1,
+    dataCriacao: helper.newDate,
+    titulo: "Conectando Bando de Dados SQL ao servidor Node.js",
+    conteudo: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    etiquetas: ['SQL', 'Banco de Dados', 'Node']
+  }
+]
 
 module.exports = posts
