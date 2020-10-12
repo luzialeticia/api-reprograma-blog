f35d76b122c6be21d6ca5b165c0b5dbd82ab671d
luzialeticia <leticialuzia@tutamail.com>
2020-10-11 23:12:43 -0300

    Criando as rotas pros gets

--- src/routes/postsRouter.js
+++ src/routes/postsRouter.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const postsController = require('../controllers/postsController')
 
+router.get('/posts', postsController.getAllPosts)
+router.get('/posts/:id', postsController.getPostByID)
 router.post('/posts', postsController.createPost)
 router.delete('/posts/:id', postsController.deletePost)
 
